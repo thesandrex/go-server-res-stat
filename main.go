29a28f45dd1542097886f78e0889509a5dfce9cd
thesandrex <aleksanya2012@yandex.ru>
2024-10-30 01:39:18 +0400

    monitoring service print fixed attempt 10

--- main.go
+++ main.go
@@ -3,112 +3,88 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 )
 
-type Stat struct {
-	LoadAvg              uint64
-	MemoryAvailable      uint64
-	MemoryUsed           uint64
-	DiskAvailable        uint64
-	DiskUsed             uint64
-	NetworkLoadAvailable uint64
-	NetworkLoadUsed      uint64
-}
+const (
+	serverURL             = "http://srv.msk01.gigacorp.local/_stats"
+	loadAvgThreshold      = 30.0
+	memoryUsageThreshold  = 80.0
+	diskUsageThreshold    = 90.0
+	networkUsageThreshold = 90.0
+	maxErrors             = 3
+)
 
 func main() {
-	url := "http://srv.msk01.gigacorp.local/_stats"
-	client := &http.Client{}
+	errCount := 0
 
 	for {
-		resp := monitorResources(url, client)
-		// fmt.Println(resp)
-		statistics := mapResponse(resp)
-		// fmt.Println(statistics)
-		evaluateStatistics(statistics)
+		if errCount >= maxErrors {
+			fmt.Println("Unable to fetch server statistic.")
+			break
+		}
 
+		response, err := http.Get(serverURL)
+		if err != nil || response.StatusCode != http.StatusOK {
+			errCount++
+			continue
+		}
+
+		defer response.Body.Close()
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			errCount++
+			continue
+		}
+
+		// Парсинг данных
+		stats := strings.Split(string(body), ",")
+		if len(stats) != 7 {
+			errCount++
+			continue
+		}
+
+		// Обнуление счётчика при успешной попытке
+		errCount = 0
+
+		loadAvg, _ := strconv.ParseFloat(stats[0], 64)
+		totalMem, _ := strconv.ParseFloat(stats[1], 64)
+		usedMem, _ := strconv.ParseFloat(stats[2], 64)
+		totalDisk, _ := strconv.ParseFloat(stats[3], 64)
+		usedDisk, _ := strconv.ParseFloat(stats[4], 64)
+		totalNet, _ := strconv.ParseFloat(stats[5], 64)
+		usedNet, _ := strconv.ParseFloat(stats[6], 64)
+
+		// Проверка Load Average
+		if loadAvg > loadAvgThreshold {
+			fmt.Printf("Load Average is too high: %.0f\n", loadAvg)
+		}
+
+		// Проверка использования памяти
+		memUsage := (usedMem / totalMem) * 100
+		if memUsage > memoryUsageThreshold {
+			fmt.Printf("Memory usage too high: %.0f%%\n", math.Floor(memUsage))
+		}
+
+		// Проверка использования дискового пространства
+		freeDisk := (totalDisk - usedDisk) / (1024 * 1024) // байты -> мегабайты
+		diskUsage := (usedDisk / totalDisk) * 100
+		if diskUsage > diskUsageThreshold {
+			fmt.Printf("Free disk space is too low: %.0f Mb left\n", math.Floor(freeDisk))
+		}
+
+		// Проверка использования сети
+		freeNet := (totalNet - usedNet) / (1000 * 1000) // байты -> мегабиты
+		netUsage := (usedNet / totalNet) * 100
+		if netUsage > networkUsageThreshold {
+			fmt.Printf("Network bandwidth usage high: %.0f Mbit/s available\n", math.Round(freeNet))
+		}
+
+		// Пауза перед следующим запросом
 		time.Sleep(5 * time.Second)
-	}
-}
-
-func monitorResources(url string, client *http.Client) string {
-	req, err := http.NewRequest("GET", url, nil)
-	throwError("Error creating request:", err)
-
-	// test
-	// return "12,4819661917,2328437675,469781716496,97858919593,1315809605,1245267076"
-
-	resp, err := client.Do(req)
-	throwError("Unable to fetch server statistic:", err)
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	throwError("Error reading response body:", err)
-
-	if resp.StatusCode == http.StatusOK {
-		return string(body)
-	} else {
-		return resp.Status
-	}
-}
-
-func throwError(msg string, err error) {
-	if err != nil {
-		fmt.Println(msg, err)
-	}
-}
-
-func mapResponse(response string) Stat {
-	elements := strings.Split(response, ",")
-	converted := convertValues(elements)
-
-	stats := Stat{
-		LoadAvg:              converted[0],
-		MemoryAvailable:      converted[1],
-		MemoryUsed:           converted[2],
-		DiskAvailable:        converted[3],
-		DiskUsed:             converted[4],
-		NetworkLoadAvailable: converted[5],
-		NetworkLoadUsed:      converted[6],
-	}
-
-	return stats
-}
-
-func convertValues(parts []string) []uint64 {
-	numbers := make([]uint64, 0, len(parts))
-
-	for _, part := range parts {
-		num, err := strconv.ParseUint(part, 10, 64)
-		throwError("Error converting to uint64:", err)
-		numbers = append(numbers, num)
-	}
-
-	return numbers
-}
-
-func evaluateStatistics(stats Stat) {
-	if stats.LoadAvg > 30.0 {
-		fmt.Printf("Load Average is too high: %d\n", stats.LoadAvg)
-	}
-
-	memUsage := (stats.MemoryUsed / stats.MemoryAvailable) * 100
-	if memUsage > 80.0 {
-		fmt.Printf("Memory usage too high: %d\n", memUsage)
-	}
-
-	freeDisk := (stats.DiskAvailable - stats.DiskUsed) / (1024 * 1024) // байты -> мегабайты
-	diskUsage := (stats.DiskUsed / stats.DiskAvailable) * 100
-	if diskUsage > 90.0 {
-		fmt.Printf("Free disk space is too low: %d Mb left\n", freeDisk)
-	}
-
-	freeNet := (stats.NetworkLoadAvailable - stats.NetworkLoadUsed) / (1000 * 1000)
-	netUsage := (stats.NetworkLoadUsed / stats.NetworkLoadAvailable) * 100
-	if netUsage > 90.0 {
-		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeNet)
 	}
 }
